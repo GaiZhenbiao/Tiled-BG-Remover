18db339e9620ebce0d387edbf7a9d2ad58e39259
Tuchuanhuhuhu <gzblog@hdu.edu.cn>
2026-02-16 12:22:53 +0800

    Upgrade to Tauri v2 dependencies and fix plugin init

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -93,6 +93,7 @@ fn crop_img(
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_opener::init())
         .manage(AppState { temp_dir: Mutex::new(None) })
         .invoke_handler(tauri::generate_handler![split_img, merge_img, crop_img])
         .run(tauri::generate_context!())
