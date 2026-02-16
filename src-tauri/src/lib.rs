4a7be31c81077fb7022d527eb6d87ccf77af6437
Tuchuanhuhuhu <gzblog@hdu.edu.cn>
2026-02-16 12:39:18 +0800

    Fix UI styling and add fs permissions for image loading

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -93,6 +93,7 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_fs::init())
         .manage(AppState { temp_dir: Mutex::new(None) })
         .invoke_handler(tauri::generate_handler![split_img, merge_img, crop_img])
         .run(tauri::generate_context!())
